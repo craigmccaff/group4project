c4e7669cac0745856120f5ff865805385dae7853
Duplon <37244663+Duplon@users.noreply.github.com>
2021-03-09 16:29:18 +0000

    initial

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
-API_KEY = "c1e412b1-5131-49f9-b7a2-bdfda4371684"
-SEARCH_NAME = input('What name would you like to search for? ')
 
-data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&name={SEARCH_NAME}").json()
-print(data)
-# skywarsWins = data["player"]["stats"]["SkyWars"]["coins"]
-# print(skywarsWins)
+def getData():
+    API_KEY = "c1e412b1-5131-49f9-b7a2-bdfda4371684"
+    SEARCH_NAME = input('What name would you like to search for? ')
+
+    data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&name={SEARCH_NAME}").json()
+    return data
+
+
+print(getData())
