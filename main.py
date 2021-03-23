383965e46de0650cc9946805a6ad53a0ddd021ea
craigmccaff <69149909+craigmccaff@users.noreply.github.com>
2021-03-23 17:18:38 +0000

    Completed tests on getGameMode

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from functions import displayData, getData, getGameMode, getKillsDeaths, getRatio, gameModeStatsDict
 
-<<<<<<< Updated upstream
 allPlayerData = getData.getData()
 selectedGame = getGameMode.getGameMode()
 
 kills = getKillsDeaths.getKills(allPlayerData, selectedGame, gameModeStatsDict.gamemodeStats)
 deaths = getKillsDeaths.getDeaths(allPlayerData, selectedGame, gameModeStatsDict.gamemodeStats)
-=======
-def searchName():
-    searchName = input('What name would you like to search for? ')
-    return searchName
->>>>>>> Stashed changes
-
 
 displayData.displayData(allPlayerData, selectedGame, gameModeStatsDict.gamemodeStats)
 
