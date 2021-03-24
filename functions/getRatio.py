3e9c18db32c3160673c33fc24c46bf43e695142c
Duplon <37244663+Duplon@users.noreply.github.com>
2021-03-24 16:57:33 +0000

    Returning the ratio to make the test works again

--- functions/getRatio.py
+++ functions/getRatio.py
@@ -3,5 +3,7 @@ def ratio(kills, deaths):
     try:
         kdratio = round(int(kills)/int(deaths), 2)
         print("KD Ratio " + str(kdratio))
+        return kdratio
     except:
-        print("KD Ratio " +kills)
+        print("KD Ratio " + kills)
+        return kills
