a1a2bea0caad712c5b91023134b6e766ddf2d7b0
JamieBurrows <80271581+JamieBurrows@users.noreply.github.com>
2021-03-30 16:09:44 +0100

    Increasing test coverage for test ratio.

--- tests/test_ratio.py
+++ tests/test_ratio.py
@@ -7,5 +7,15 @@ class Test(TestCase):
     def test_ratio(self):
         self.assertEqual(getRatio.ratio(20,10), "KD Ratio 2.0")
 
+    def test_ratio_no_deaths(self):
+        self.assertEqual(getRatio.ratio(10,0),"KD Ratio 10")
 
+    def test_ratio_no_kills(self):
+        self.assertEqual(getRatio.ratio(0,1),"KD Ratio 0.0")
+
+    def test_ratio_no_kills_or_deaths(self):
+        self.assertEqual(getRatio.ratio(0,0),"KD Ratio 0")
+
+if __name__ == '__main__':
+    unittest.main()
 
