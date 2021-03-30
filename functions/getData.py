aefa63d923580810a6886ac97c65b4eaf58030a2
Duplon <37244663+Duplon@users.noreply.github.com>
2021-03-30 15:40:18 +0100

    Added the option to read from a JSON file

--- functions/getData.py
+++ functions/getData.py
@@ -1,16 +1,20 @@
-from functions import getSearchName, writeDataToJson
+from functions import getSearchName, writeDataToJson, returnJsonAsDict
 import requests
 
 
 def getData():
     API_KEY = "c1e412b1-5131-49f9-b7a2-bdfda4371684"
-
-    flag = True
-    while flag:
-        name = getSearchName.searchName()
-        data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&name={name}").json()
-        if data['success'] == True:
-            writeDataToJson.writeFile(data)
-            return data
-        else:
-            print("You have recently searched for this name, please wait before searching again.")
+    choice = input("Type <JSON> to load from JSON otherwise it'll use the API ")
+    if choice.lower() == "json":
+        data = returnJsonAsDict.getRecentSearch()
+        return data
+    else:
+        flag = True
+        while flag:
+            name = getSearchName.searchName()
+            data = requests.get(f"https://api.hypixel.net/player?key={API_KEY}&name={name}").json()
+            if data['success'] == True:
+                writeDataToJson.writeFile(data)
+                return data
+            else:
+                print("You have recently searched for this name, please wait before searching again.")
